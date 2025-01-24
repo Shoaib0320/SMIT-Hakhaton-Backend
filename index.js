6d30627d6f76991c5df3b8efbaae160bd97fae86
Shoaib Raza <hafizshoaibraza180@gmail.com>
2025-01-24 15:41:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// Import Express
+// Import Express
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import usersRoutes from "./routers/userRoute.js";
+import { connectDB } from "./lib/DB/connectDB.js";
+
+const app = express(); // Express App Create Karo
+const PORT = 1222; // Port Define Karo
+
+// Load environment variables from .env.local
+dotenv.config(); // Load .env variables
+
+// Middleware (JSON Data Handle Karne Ke Liye)
+app.use(express.json());
+
+// Connect to the database
+connectDB();
+
+app.use(
+	cors({
+	  origin: [
+		"http://localhost:5173",  // Allow localhost for development
+		"https://manegement-system.vercel.app",  // Allow Vercel URL for production
+	  ], // Allow these specific frontend addresses
+	  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+	  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+	  credentials: true,              // Allows cookies to be sent
+	})
+  );
+  
+
+// Routes Api
+app.use("/users", usersRoutes);
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js Backend!');
+});
+
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
